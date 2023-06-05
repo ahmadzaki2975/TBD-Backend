717cf6576b8e6f0ac37a726756d02358b265bf14
Ahmad Zaki Akmal <ahmad.zaki.akmal@mail.ugm.ac.id>
2023-06-05 07:18:46 +0700

    feat: add author controller

---
+++ controllers/author.js
@@ -0,0 +1,31 @@
+const knex = require("knex");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const db = knex({
+  client: "pg",
+  connection: {
+    host: process.env.POSTGRES_HOST,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
+  },
+});
+
+exports.getAuthors = (req, res) => {
+    try {
+        const query = `
+        SELECT * FROM Author`;
+        db.raw(query)
+        .then((data) => {
+            console.log(data.rows);
+            res.status(200).json(data.rows);
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+    } catch (err) {
+        console.log(err);
+    }
+}
