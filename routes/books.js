4961422903532010e6c0c4fca57f07ef6f79e49a
Ahmad Zaki Akmal <ahmad.zaki.akmal@mail.ugm.ac.id>
2023-06-05 20:12:39 +0700

    feat: add update book route

--- routes/books.js
+++ routes/books.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
-const { getBooks, getBookById } = require('../controllers/book');
+const { getBooks, getBookById, updateBookById } = require('../controllers/book');
 
 router.get('/', getBooks);
 router.get('/:id', getBookById);
+router.post('/update/:id', updateBookById);
 
 module.exports = router;
