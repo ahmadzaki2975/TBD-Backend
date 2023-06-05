4a49da851753042d670f28580068c6a3a25251f7
Ahmad Zaki Akmal <ahmad.zaki.akmal@mail.ugm.ac.id>
2023-06-06 06:48:05 +0700

    feat: add controller and route for genres

---
+++ controllers/genres.js
@@ -0,0 +1,29 @@
+const knex = require("knex");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const db = knex({
+  client: "pg",
+  connection: {
+    host: process.env.POSTGRES_HOST,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
+  },
+});
+
+exports.getGenres = (req, res) => {
+  try {
+    const query = `SELECT * FROM Genre`;
+    db.raw(query)
+      .then((data) => {
+        res.status(200).json(data.rows);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  } catch (err) {
+    console.log(err);
+  }
+};
