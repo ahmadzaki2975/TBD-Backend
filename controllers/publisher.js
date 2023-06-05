468f4386c79138c7107c4c0ea73c1c8eb3773b9b
Ahmad Zaki Akmal <ahmad.zaki.akmal@mail.ugm.ac.id>
2023-06-05 08:35:08 +0700

    feat: create publisher controller

---
+++ controllers/publisher.js
@@ -0,0 +1,32 @@
+const knex = require("knex");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const db = knex({
+  client: "pg",
+  connection: {
+    host: process.env.POSTGRES_HOST,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
+  },
+});
+
+exports.getPublishers = (req, res) => {
+    const query = `
+    SELECT * FROM Publisher`;
+
+    try {
+        db.raw(query)
+        .then((data) => {
+            console.log(data.rows);
+            res.status(200).json(data.rows);
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+    } catch (err) {
+        console.log(err);
+    }
+}
