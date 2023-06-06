477edea4a3f9574d94fa6620099ea9dc7ce0df63
Ahmad Zaki Akmal <ahmad.zaki.akmal@mail.ugm.ac.id>
2023-06-07 05:25:14 +0700

    feat: add publisher routes

--- routes/publisher.js
+++ routes/publisher.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
-const { getPublishers } = require('../controllers/publisher');
+const { getPublishers, getPublisherById, deletePublisherById } = require('../controllers/publisher');
 
 router.get('/', getPublishers);
+router.get('/:publishername', getPublisherById);
+router.delete('/delete/:publishername', deletePublisherById);
 
 module.exports = router;
