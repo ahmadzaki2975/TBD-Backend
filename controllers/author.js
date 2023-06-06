d3fe090cfd47d967b450629413e6b3424abe6226
Ahmad Zaki Akmal <ahmad.zaki.akmal@mail.ugm.ac.id>
2023-06-06 11:46:58 +0700

    feat: add getWrittenBooks controller

--- controllers/author.js
+++ controllers/author.js
@@ -13,36 +13,56 @@ const db = knex({
 });
 
 exports.getAuthors = (req, res) => {
-    try {
-        const query = `
+  try {
+    const query = `
         SELECT * FROM Author`;
-        db.raw(query)
-        .then((data) => {
-            res.status(200).json(data.rows);
-        })
-        .catch((err) => {
-            console.log(err);
-            res.status(500).json(err);
-        });
-    } catch (err) {
+    db.raw(query)
+      .then((data) => {
+        res.status(200).json(data.rows);
+      })
+      .catch((err) => {
         console.log(err);
-    }
-}
+        res.status(500).json(err);
+      });
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 exports.getAuthorById = (req, res) => {
-    const { id } = req.params;
-    try {
-        const query = `
+  const { id } = req.params;
+  try {
+    const query = `
         SELECT * FROM Author WHERE AuthorID = ${id}`;
-        db.raw(query)
-        .then((data) => {
-            res.status(200).json(data.rows);
-        })
-        .catch((err) => {
-            console.log(err);
-            res.status(500).json(err);
-        });
-    } catch (err) {
+    db.raw(query)
+      .then((data) => {
+        res.status(200).json(data.rows);
+      })
+      .catch((err) => {
         console.log(err);
-    }
-}
+        res.status(500).json(err);
+      });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+exports.getWrittenBooks = (req, res) => {
+  const { id } = req.params;
+  try {
+    const query = `SELECT * FROM Book WHERE 
+        BookID IN (SELECT BookID FROM BookAuthorMapping WHERE AuthorID = ${id});`;
+    db.raw(query)
+      .then((data) => {
+        res.status(200).json(data.rows);
+        return;
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+        return;
+      });
+  } catch (err) {
+    console.log(err);
+  }
+};
