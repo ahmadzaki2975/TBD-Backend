bf015815de2dee427948dd034e82e048572d5611
Ahmad Zaki Akmal <ahmad.zaki.akmal@mail.ugm.ac.id>
2023-06-06 19:22:50 +0700

    feat: add postNewAuthor route

--- routes/author.js
+++ routes/author.js
@@ -1,9 +1,10 @@
 const router = require('express').Router();
-const { getAuthors, getAuthorById, getWrittenBooks } = require('../controllers/author');
+const { getAuthors, getAuthorById, getWrittenBooks, postNewAuthor } = require('../controllers/author');
 
 router.get('/', getAuthors);
 router.get('/:id', getAuthorById);
 router.get('/books/:id', getWrittenBooks);
+router.post('/add', postNewAuthor);
 
 
 module.exports = router;
